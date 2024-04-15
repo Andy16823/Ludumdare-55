5396825ee7dd57c92cf619c25049a765000c443f
Andy16823 <wagnerandreas17@yahoo.de>
2024-04-15 12:45:49 +0200

    Update

--- Summoning/Launcher.Designer.cs
+++ Summoning/Launcher.Designer.cs
@@ -79,7 +79,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 168);
+            this.label2.Location = new System.Drawing.Point(12, 172);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 13);
             this.label2.TabIndex = 5;
@@ -87,8 +87,9 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(160, 154);
+            this.trackBar1.Location = new System.Drawing.Point(160, 164);
             this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 90;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(290, 45);
             this.trackBar1.TabIndex = 6;
